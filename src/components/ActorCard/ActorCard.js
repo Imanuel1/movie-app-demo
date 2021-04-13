9132e2b58d7b23062eae428d86fcfa9b1b4f725b
imanuel <toswim100@gmail.com>
2021-04-13 15:36:23 +0300

    update the actorData that it'll get an ActorModel object

--- src/components/ActorCard/ActorCard.js
+++ src/components/ActorCard/ActorCard.js
@@ -6,11 +6,11 @@ export default function ActorCard({actor}) {
     return (
     <div className="col-xl-3 col-lg-4 col-sm-6 col-xs-12 c-actor-card">
         <Card style={{ width: '16rem'}}>
-            <Card.Img variant="top" src={actor.image} />
+            <Card.Img variant="top" src={actor.img} />
             <Card.Body>
                 <Card.Title>
                     <a href={actor.imdb} target="_blank">
-                        {`${actor["first-name"]} ${actor["last-name"]}`}
+                        {`${actor["fname"]} ${actor["lname"]}`}
                     </a>
                 </Card.Title>
                 <Card.Text>
