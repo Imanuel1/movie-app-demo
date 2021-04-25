b87df034d00683b9dfd5472c2c477630592735a8
imanuel <toswim100@gmail.com>
2021-04-25 13:01:56 +0300

    add Not Found Page + add Router inside App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ActorsPage from './pages/ActorsPage/ActorsPage';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return(
       <div className="App">
-        <ActorsPage/>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/">Hello Actors</Route>
+            <Route exact path="/actors"><ActorsPage/></Route>
+            <Route path="/"></Route>
+          </Switch>
+        </HashRouter>
      </div>
   )
 }
