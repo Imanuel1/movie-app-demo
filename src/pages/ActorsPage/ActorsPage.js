23156c45a8e29a5bfe1a0465bb814466e49fb6f9
imanuel <toswim100@gmail.com>
2021-04-25 13:50:43 +0300

    sperate Movies page from ActorsPage.js

--- src/pages/ActorsPage/ActorsPage.js
+++ src/pages/ActorsPage/ActorsPage.js
@@ -110,55 +110,7 @@ export default function ActorsPage() {
         }
     });
 
-    //add movie search section
-    const [movies, setMovies] = useState([]);
-    const [searchText, setSearchText] = useState("");
-    const [results, setResults] = useState([]);
-
-    function handleSearchChange(newSearchText) {
-        setSearchText(newSearchText);
-
-        if(newSearchText){
-            const searchURL = "https://api.themoviedb.org/3/search/movie?api_key=09848168e2201726ba2c5e6a70d11ac7&language=en-US&query=" + newSearchText;
-            axios.get(searchURL).then( response =>
-                setResults(response.data.results)
-            );
-        } else {
-            setResults([]);
-        }
-    }
     
-    function addMovie(index) {
-        const movieId = results[index].id;
-        console.log("movie id: "+movieId);
-        const searchURL = "https://api.themoviedb.org/3/movie/"+movieId+"?api_key=09848168e2201726ba2c5e6a70d11ac7&language=en-US";
-        let MovieLength=0;
-        let img="";
-        let actor =[];
-        let director =[];
-        axios.get(searchURL).then(response =>{
-            MovieLength= response.data.runtime;
-            console.log(MovieLength);
-            setMovies(movies.concat(new MovieModel(results[index].title, MovieLength ,img,director,actor)));
-        });
-        axios.get("https://api.themoviedb.org/3/movie/"+movieId+"/images?api_key=09848168e2201726ba2c5e6a70d11ac7&language=en-US,null").then(response => {
-            img= response.data.posters[0]?response.data.posters[0].file_path:response.data.backdrops[0].file_path;
-            console.log(img);
-            console.log(response.data);
-            setMovies(movies.concat(new MovieModel(results[index].title, MovieLength ,img,director,actor)));
-        });
-
-        axios.get("https://api.themoviedb.org/3/movie/"+movieId+"/credits?api_key=09848168e2201726ba2c5e6a70d11ac7&language=en-US").then(response => {
-            actor= response.data.cast.filter((person, index) => index > 5 ?null:person.name +" ("+ person.character+")");
-            director = response.data.crew.filter((person) => person.known_for_department ==="Directing" ? person.name: null)
-            console.log(actor);
-            console.log(director);
-            setMovies(movies.concat(new MovieModel(results[index].title, MovieLength ,img,director,actor)));
-        });
-
-        setResults([]);
-        setSearchText("");
-    }
 
     return (
         <Container className="p-actors">
@@ -176,15 +128,6 @@ export default function ActorsPage() {
                 </Form.Row>
                 <Row>
                    {filteredActors.map((actor, index) => <Col lg={3} md={6}><ActorCard key={index} actor={actor}/></Col>)}
-                </Row>
-                <SearchBox
-                    placeholder="Search a movie..."
-                    searchText={searchText}
-                    onSearchChange={handleSearchChange}
-                    results={results.map(result => result.original_title)}
-                    onResultSelected={addMovie}/>
-                <Row className="movie-row">
-                    {movies.map(movie => <Col lg={12}><MovieCard movie={movie}/></Col>)}
                 </Row>
         </Container>
     )
